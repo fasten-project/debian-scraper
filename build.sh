7ac1a3fa14f549aa89d892e0a91026dd05b63c67
StefanosChaliasos <stefanoshaliassos@gmail.com>
2021-03-31 19:24:56 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 cd udd-mirror
 sudo docker build --no-cache -t debian-scraper -f Dockerfile .
-sudo docker tag debian-scraper schaliasos/debian-scraper
+sudo docker tag debian-scraper \
+    docker.pkg.github.com/fasten-project/debian-scraper/schaliasos/debian-scraper
+cd ../
+cd udd-mirror
+sudo docker build --no-cache -t kafka-dscanalyzer -f Dockerfile .
+sudo docker tag kafka-dscanalyzer \
+    docker.pkg.github.com/fasten-project/debian-scraper/kafka-dscanalyzer
 cd ../
